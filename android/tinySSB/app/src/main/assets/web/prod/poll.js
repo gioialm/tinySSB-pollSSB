70a2453cbc4b0aa81c230e9b1fc49b8fd8fb936d
Gioia Almer <gioia.almer@stud.unibas.ch>
2025-07-15 09:38:24 +0200

    Implements that min 2 options must be filled out to send a poll

--- android/tinySSB/app/src/main/assets/web/prod/poll.js
+++ android/tinySSB/app/src/main/assets/web/prod/poll.js
@@ -66,6 +66,11 @@ function submit_poll_creator() {
         return;
     }
 
+    if (options.length < MIN_OPTIONS) {
+        launch_snackbar("Please enter at least " + MIN_OPTIONS + " options.");
+        return;
+    }
+
     const lowercaseOptions = options.map(opt => opt.toLowerCase());
     const uniqueOptions = new Set(lowercaseOptions);
     if (uniqueOptions.size !== lowercaseOptions.length) {
